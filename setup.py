a5963e0e0c7ed29b20d5d78d36676e7957f2734c
weiensong <wes@shoot.net.cn>
2023-11-23 12:04:05 +0800

    fix: setup

--- setup.py
+++ setup.py
@@ -1,34 +1,26 @@
-import os
-
 from setuptools import setup, find_packages
 
 
 setup(
-    name='EasierDocker',
+    name='easier_docker',
     version='1.0.0',
     author='weiensong',
     author_email='touer0018@gmail.com',
-    description='A brief description of your package',  # todo
+    description='A python package that makes it easier for you to use local docker.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/your_package_name',  # todo
+    url='https://github.com/weiensong/easierdocker',
     packages=find_packages(),
     install_requires=[
             'docker~=6.1.3',
     ],
-    entry_points={
-        'console_scripts': [
-            'your_script_name = your_package_name.your_module:main',
-        ],
-    },
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
     ],
     keywords='easy, docker, docker sdk, python docker',
-    # todo
     project_urls={
-        'Bug Reports': 'https://github.com/yourusername/your_package_name/issues',
-        'Source': 'https://github.com/yourusername/your_package_name',
+        'Bug Reports': 'https://github.com/weiensong/easier_docker/issues',
+        'Source': 'https://github.com/weiensong/easierdocker',
     },
 )
