6bd2e1ccf8973c56cbf13f4cd45f9b5e8b966d9e
weiensong <wes@shoot.net.cn>
2024-01-17 15:23:42 +0800

    feat: pull the not exists docker image exception

--- easierdocker/exceptions.py
+++ easierdocker/exceptions.py
@@ -2,3 +2,9 @@ class DockerConnectionError(Exception):
     def __init__(self, message="Unable to connect to Docker server, please make sure dockers is running."):
         self.message = message
         super().__init__(self.message)
+
+
+class NotFoundImageInDockerHub(Exception):
+    def __init__(self, image_name: str):
+        self.message = f'Unable to pull the image named [{image_name}], please confirm whether it exists'
+        super().__init__(self.message)
