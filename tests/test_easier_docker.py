ef55927be9822a0f2b0d45187f33ab3d319cdce7
touero <touer0018@gmail.com>
2024-04-22 22:58:58 +0800

    fix: add test

--- tests/test_easier_docker.py
+++ tests/test_easier_docker.py
@@ -70,3 +70,29 @@ class TestEasierDocker(unittest.TestCase):
         self.assertEqual(easier_docker.client, mock_client)
         self.assertEqual(easier_docker.image_name, container_config['image'])
         self.assertEqual(easier_docker.container_name, container_config['name'])
+
+    @patch('docker.from_env')
+    def test_get_images(self, mock_from_env):
+        parent_dir = os.path.dirname(os.getcwd())
+        host_script = os.path.join(parent_dir, 'example')
+        container_script = '/path/to/container'
+        container_config = {
+            'image': 'python:3.9',
+            'name': 'python_test',
+            'volumes': {
+                f'{host_script}': {'bind': container_script, 'mode': 'rw'}
+            },
+            'detach': True,
+            'command': ["sh", "-c", f'cd {container_script} &&'
+                                    'python docker_example.py'],
+        }
+
+        network_config = {
+            'name': 'bridge',
+            'driver': 'bridge',
+        }
+        mock_client = MagicMock()
+        mock_from_env.return_value = mock_client
+        easier_docker = EasierDocker(container_config=container_config, network_config=network_config)
+        easier_docker._EasierDocker__get_image()
+        mock_client.images.get.assert_called_once_with(easier_docker.image_name)
